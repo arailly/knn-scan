5178b53b36b6b987830e1e4c754907e046bab8a3
arailly <yusuke.arailly@gmail.com>
2021-03-19 16:32:26 +0900

    :+1: input from command line

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace cpputil;
 
-auto save(const string &path, vector <Neighbors> results) {
+auto save(const string &path, vector<Neighbors> results) {
     ofstream ofs(path);
 
     if (!ofs) throw runtime_error("invalid path");
@@ -21,14 +21,26 @@ auto save(const string &path, vector <Neighbors> results) {
     }
 }
 
-int main() {
-    const int n = 1000000, n_query = 10;
-    const int dim = 128;
-    int k = 5, k_max = 100;
+int main(int argc, char **argv) {
+    if (argc != 8) {
+        cout << argv[0] << " "
+                           "k "
+                           "dim "
+                           "n "
+                           "n_query"
+                           "dataset_path "
+                           "query_path "
+                           "save_path" << endl;
 
-    const string data_path = "/mnt/qnap/data/sift/sift_base.fvecs";
-    const string query_path = "/mnt/qnap/data/sift/sift_query.fvecs";
-    const string save_path = "/home/arai/workspace/sample.csv";
+    }
+
+    int k = atoi(argv[1]);
+    int dim = atoi(argv[2]);
+    int n = atoi(argv[3]);
+    int n_query = atoi(argv[4]);
+    string data_path(argv[5]);
+    string query_path(argv[6]);
+    string save_path(argv[7]);
 
     auto dataset = DataArray(n, dim);
     dataset.load(data_path);
@@ -36,13 +48,20 @@ int main() {
     auto queries = DataArray(n_query, dim);
     queries.load(query_path);
 
-    vector <Neighbors> results(n_query);
+    vector<Neighbors> results(n_query);
+
+    const auto start = get_now();
 
 #pragma omp parallel for
     for (int query_id = 0; query_id < n_query; ++query_id) {
         const auto query = queries.find(query_id);
         results[query_id] = knn_scan(k, query, dataset);
     }
+
+    const auto stop = get_now();
+
+    cout << "search time: " << get_duration(start, stop) / 1000000
+         << "[s]" << endl;
 
     save(save_path, results);
 }
