b61abf733f242f676eee39cb84dea9a7496207a9
arailly <yusuke.arailly@gmail.com>
2021-03-22 15:45:44 +0900

    :+1: add test on sift

--- test/test.cpp
+++ test/test.cpp
@@ -3,3 +3,29 @@
 
 using namespace std;
 using namespace cpputil;
+
+TEST(knn_scan, sift) {
+    int k = 10, dim = 128, k_max = 100;
+    int n = 1000000, n_query = 2;
+    string data_path = "/mnt/qnap/data/sift/sift_base.fvecs";
+    string query_path = "/mnt/qnap/data/sift/sift_query.fvecs";
+    string gt_path = "/mnt/qnap/data/sift/sift_groundtruth.ivecs";
+
+    auto db = DataArray(n, dim);
+    db.load(data_path);
+
+    auto queries = DataArray(n_query, dim);
+    queries.load(query_path);
+
+    auto gt = GroundTruth(n_query, k_max);
+    gt.load(gt_path);
+
+    for (int query_id = 0; query_id < n_query; ++query_id) {
+        const auto query = queries.find(query_id);
+        const auto result = knn_scan(k, query, db);
+
+        for (int i = 0; i < k; ++i) {
+            ASSERT_EQ(result[i].id, gt[query_id][i]);
+        }
+    }
+}
